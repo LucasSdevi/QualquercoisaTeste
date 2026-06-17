dbb04dc981a47ffb4b36f0dee2b174ad37d1316b
Lucas <lucas_silvafagundes@hotmail.com>
2026-06-16 22:15:20 -0300

    Teste de commit

---
+++ src/main/java/com/ninjas/demo/NinjaModel.java
@@ -0,0 +1,34 @@
+package com.ninjas.demo;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+
+@Entity
+@Table(name = "tb_cadastro_de_ninjas")
+public class NinjaModel {
+
+    @Id
+    @GeneratedValue
+    Long id;
+    String nome;
+    String email;
+    int idade;
+
+    public NinjaModel() {
+    }
+
+    public NinjaModel(Long id, String nome, String email, int idade) {
+    this.id = id;
+    this.nome = nome;
+    this.email = email;
+    this.idade = idade;
+}    
+
+
+
+
+}
+
+
